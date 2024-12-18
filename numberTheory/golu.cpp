044552d28b5a57485eef5bf2991040edd091b874
Nitin Sharma <nitinsharmaa787@gmail.com>
2024-12-18 22:34:27 +0530

    Add brute force Euler's totient function; implemented prime checking in golu.cpp

--- numberTheory/golu.cpp
+++ numberTheory/golu.cpp
@@ -15,15 +15,7 @@ typedef vector<ll> vll;
 typedef vector<string> vs;
 typedef vector<ull> vull;
 
-// brute force O(n long n)
-
-int phi_b(int n) {
-    int ans = 0;
-    FOR(i, 1, n) {
-        if (gcd(i,n) == 1) ans++;
-    }
-    return ans;
-}
+// https://www.codechef.com/problems/COZIE  (need optimization for submiting)
 
 int phi(int n) {
     int ans = n;
@@ -39,10 +31,32 @@ int phi(int n) {
     return ans;
 }
 
+bool is_prime(int n) {
+    if (n==1) return false;
+    if (n==2) return true;
+    for (int i = 2; i*i <=n; i++) {
+        if (n%i == 0) return false;
+    }
+    return true;
+}
+
 void solve() {
-    int n;
-    cin >> n;
-    cout << phi_b(n) << endl;
+    int n, q;
+    cin >> n >> q;
+    vi a(n);
+    FOR(i,1,n+1) cin >> a[i];
+    vi pre(n+1, 0);
+
+    FOR(i,1,n+1) {
+        pre[i] = pre[i-1];
+        if (is_prime(phi(a[i]))) 
+            pre[i]++;
+    }
+    int l, r;
+    FOR(i,0,q) {
+        cin >> l >> r;
+        cout << pre[r] - pre[l-1] << endl;
+    }
 }
 
 int main () {
@@ -50,7 +64,7 @@ int main () {
     cin.tie(NULL);
     cout.tie(NULL);
     int t = 1;
-    cin >> t;
+    // cin >> t;
     while (t-- > 0) {
         solve();
     }
