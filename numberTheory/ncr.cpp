9b80c013112f63354cc6e94071f1f73421f1dcc5
Nitin Sharma <nitinsharmaa787@gmail.com>
2025-01-12 12:31:37 +0530

    update function names and add nCr implementation in template.cpp

--- numberTheory/ncr.cpp
+++ numberTheory/ncr.cpp
@@ -20,8 +20,35 @@ typedef vector<ull> vull;
 typedef unordered_set<int> usi;
 const int MOD =  1e9+7;
 
-void Pechkas69() {
+ll pow(int a, int b) {
+    if (b==0) return 1;
+    ll ans = pow(a, b/2);
+    ans = (ans*ans)%MOD;
+    if (b%2 == 1) ans = (ans*a)%MOD;
+    return ans;
+}
 
+ll mod_inv(int a) {
+    return pow(a, MOD-2);
+}
+
+ll fact(int n) {
+    ll ans = 1;
+    FOR(i,2,n+1) ans = (ans*i)%MOD;
+    return ans;
+}
+
+ll ncr(int n, int r) {
+    ll nf = fact(n);
+    ll nrf = mod_inv(fact(n-r));
+    ll rf = mod_inv(fact(r));
+    return (rf*((nf*nrf)%MOD))%MOD;
+}
+
+void Pechkas69() {
+    int n, r;
+    cin >> n >> r;
+    cout << ncr(n, r) << endl;
 }
 
 int main () {
